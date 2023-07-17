1975261218fda3a9f296c4b48e37b9e2baca7306
Richard T <richard@turing.edu>
2023-07-17 11:37:21 -0400

    Migrations

---
+++ MvcMovie/Migrations/20230717153320_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    genre = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_movies", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "movies");
+        }
+    }
+}
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -2,5 +2,8 @@
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
     }
 }
