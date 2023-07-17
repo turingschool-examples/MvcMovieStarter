3759dea8b0f0069687a8b2e683c21c1a634e15bd
Richard T <richard@turing.edu>
2023-07-17 11:28:27 -0400

    Movies controller and Index view

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //GET:/movies/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
