de428f93383a8a3196fb4a46e96c914a7636eb04
Richard T <richard@turing.edu>
2023-07-17 14:33:44 -0400

    Test project added

---
+++ MvcMovie.FeatureTests/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvcMovie.Controllers;
+
+namespace MvcMovie.FeatureTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddControllersWithViews()
+                            .AddApplicationPart(typeof(HomeController).Assembly);
+                    });
+
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllerRoute(
+                                name: "default",
+                                pattern: "{controller=Home}/{action=Index}/{id?}");
+                        });
+                    });
+                });
+    }
+}
---
+++ MvcMovie.FeatureTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MvcMovie.FeatureTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
