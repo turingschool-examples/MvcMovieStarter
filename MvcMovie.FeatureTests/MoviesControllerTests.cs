851542f0a90dfcd2146f5644fe93141c30434f62
Zoe Farrell <zoe@turing.edu>
2023-07-17 10:32:44 -0600

    Add Show Route

---
+++ MvcMovie.FeatureTests/MoviesControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.DataAccess;
+using MvcMovie.Models;
+using NuGet.Frameworks;
+
+namespace MvcMovie.FeatureTests
+{
+    public class MovieControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public MovieControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        private MvcMovieContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MvcMovieContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new MvcMovieContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        [Fact]
+        public async Task Index_ReturnsViewWithMovies()
+        {
+            //Arrange
+            //Set up the client
+            var client = _factory.CreateClient();
+            //Get Spaceballs and Young Frankenstein in the DB
+            var context = GetDbContext();
+            context.Movies.Add(new Movie { Genre = "Comedy", Title = "Spaceballs" });
+            context.Movies.Add(new Movie { Genre = "Comedy", Title = "Young Frankenstein" });
+            context.SaveChanges();
+
+            //Act
+            // Make a get request to /movies and save the raw response
+            var response = await client.GetAsync("/movies");
+            //Turn the raw response into a string
+            var html = await response.Content.ReadAsStringAsync();
+
+            //Assert
+            //Assert that the status is success
+            response.EnsureSuccessStatusCode();
+            //Assert that the page contains spaceballs
+            Assert.Contains("Spaceballs", html);
+            //Assert that the page contains young frankenstein
+            Assert.Contains("Young Frankenstein", html);
+            //Assert that Elf is not in the page
+            Assert.DoesNotContain("Elf", html);
+        }
+    }
+}
--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -5,7 +5,6 @@ namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
-
         private readonly MvcMovieContext _context;
 
         public MoviesController(MvcMovieContext context)
@@ -17,6 +16,14 @@ namespace MvcMovie.Controllers
         {
             var movies = _context.Movies;
             return View(movies);
+        }
+
+        // GET: /Movies/<id>
+        [Route("Movies/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            return View(movie);
         }
     }
 }
