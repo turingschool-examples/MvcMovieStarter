56222aeb3face0651a8f118cec523759c94030a5
Richard T <richard@turing.edu>
2023-07-17 11:48:28 -0400

    Show route and view

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.DataAccess;
 
 namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
-        //GET:/movies/
+        private readonly MvcMovieContext _context;
+
+        public MoviesController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Movies/
+
         public IActionResult Index()
         {
+            foreach (var movie in _context.Movies)
+            {
+                Console.WriteLine($"Movie {movie.Id}, Title: {movie.Title}, Genre {movie.Genre}");
+            }
             return View();
+        }
+
+        // GET: /Movies/<id>
+        [Route("Movies/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            return View(movie);
         }
     }
 }
